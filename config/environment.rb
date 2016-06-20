015042abec4bbb76e8a6b1d6fb31416ebd0188a5
Glenn Loontjens <gclhome@yahoo.com>
2016-06-20 21:07:29 +0000

    added devise gem

--- config/environment.rb
+++ config/environment.rb
@@ -13,3 +13,19 @@ ActionMailer::Base.smtp_settings = {
   :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
+
+
+
+# config.action_mailer.default_url_options = { :host => 'https://coder-manual-rails-gloontjens.c9users.io' }
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.perform_deliveries = true
+# config.action_mailer.smtp_settings = {
+#     :address              => 'smtp.sendgrid.net',
+#     :port                 => 2525,
+#     :domain               => 'c9.io',
+#     :user_name            => ENV['SENDGRID_USERNAME'],
+#     :password             => ENV['SENDGRID_PASSWORD'],
+#     :authentication => :plain,
+#     :enable_starttls_auto => true,
+#     :ssl =>false
+# }
